3c0bf24cccde2302e2cacea147f116a7dfd41967
Matthew Bull <matt_wings@hotmail.com>
2022-02-22 15:51:40 +0000

    Add some navigation

--- src/navigator.ts
+++ src/navigator.ts
@@ -1,4 +1,16 @@
-export const navigate = (start: [number, number]) => {
+type Direction = 'F' | 'B' | 'L' | 'R';
+
+const directionToTranslation: Record<Direction, [number, number]> = {
+  F: [1, 0],
+  B: [-1, 0],
+  L: [0, -1],
+  R: [0, 1],
+};
+
+
+export const navigate = (start: [number, number], directions: Direction[] = []) => {
   if (!start || start.length !== 2)
     throw new Error('Missing start point');
+
+  return directions.map(x => directionToTranslation[x]).reduce(([totX, totY], [x, y]) => [totX + x, totY + y], start);
 };
--- test/navigator.spec.ts
+++ test/navigator.spec.ts
@@ -5,4 +5,10 @@ describe('navigator', () => {
   it('requires a start point', () => {
     expect(() => (navigate as any)()).to.throw('Missing start point');
   });
+  it('returns the start point with no directions', () => {
+    expect(navigate([0, 0])).to.deep.equal([0, 0]);
+  });
+  it('moves up one when told to move forward 1', () => {
+    expect(navigate([0, 0], ['F'])).to.deep.equal([1, 0]);
+  });
 });
